5a04b3fc7a43e1d26ed2e03d80f676ebe6e04e8d
prodhc <zionywood@gmail.com>
2021-06-08 15:39:35 -0700

    cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments()
+
+        tbl.string('vin', 17).notNullable().unique()
+
+        tbl.string('make', 128).notNullable()
+
+        tbl.string('model', 128).notNullable()
+
+        tbl.numeric('mileage').unsigned().notNullable()
+
+        tbl.string('title', 128)
+
+        tbl.string('transmission', 128)
+    })
+}
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+{
+    vin: '11111111111111111',
+    make: "toyota",
+    model: "corolla",
+    mileage: 100000,
+    title: "clean",
+    transmission: "manuual"
+},
+{
+    vin: '11111111111111111',
+    make: "ford",
+    model: "focus",
+    mileage: 234988,
+},
+{
+    vin: '11111111111111111',
+    make: "honda",
+    model: "civic",
+    mileage: 10456,
+    title: "reconstructed",
+    transmission: "auto"
+}
+]
+
+exports.seed = function(knex) {
+    return knex('cars').truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
