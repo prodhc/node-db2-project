18dc41f7ab3642f40afc21ac28d7a76071fabf25
prodhc <zionywood@gmail.com>
2021-06-08 21:27:10 -0700

    testing 2

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use('*', (req, res, next) => {
 })
 
 server.use((err, req, res, next) => {
-    res.status(err.status || 400).json({
+    res.status(err.status || 500).json({
         message: err.message
     })
 })
